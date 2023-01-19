c619c07ef5555ecb41ffa8fd0125c945af2c0f0b
Michael Chiang <michael.chiang.mc5@gmail.com>
2023-01-18 22:16:32 -0800

    implemented moving nodes

--- src/devTools/components/Node.svelte
+++ src/devTools/components/Node.svelte
@@ -9,11 +9,37 @@
   const { nodesStore, edgesStore, anchorsStore, onMouseMove } =
     findStore(canvasId);
 
-  const node = $nodesStore[nodeId];
+  let node;
+  $: node = $nodesStore[nodeId];
   console.log(nodeId);
+
+  let isSelected = false;
 </script>
 
+<svelte:window
+  on:mousemove={(e) => {
+    e.preventDefault();
+    if (isSelected) {
+      onMouseMove(e, node.id);
+    }
+  }}
+  on:mouseup={(e) => {
+    // in theory, the mouse should always stay within the component and never go outside so this is unnecessary
+    // in practice, there may be a bug and the cursor may leave the component while moving it, and then the
+    // mouseup event won't be detected
+    isSelected = false;
+  }}
+/>
+
 <div
+  on:mousedown={(e) => {
+    isSelected = true;
+    // $nodeIdSelected = node.id;
+    // $nodeSelected = true;
+  }}
+  on:mouseup={(e) => {
+    isSelected = false;
+  }}
   style="left: {node.positionX}px;
     top: {node.positionY}px;
     width: {node.width}px;
